cca9f624d27a86b5333d2e60885314cefc8d03c2
Wang Kai <kaiwkx@gmail.com>
2018-07-13 10:33:14 +0800

    Fix: rename private variable prefix, breakdown code into functions

--- slim-modeline.el
+++ slim-modeline.el
@@ -37,25 +37,77 @@
   :type 'string
   :group 'slim-modeline)
 
-(defvar smm-foreground-backup nil
+(defvar smm--foreground-backup nil
   "Mode line foreground backup.")
 
-(defvar smm-background-backup nil
+(defvar smm--background-backup nil
   "Mode line background backup.")
 
-(defvar smm-inactive-foreground-backup nil
+(defvar smm--inactive-foreground-backup nil
   "Mode line inactive foreground backup.")
 
-(defvar smm-inactive-background-backup nil
+(defvar smm--inactive-background-backup nil
   "Mode line inactive background backup.")
 
-(defvar smm-height-backup nil
+(defvar smm--height-backup nil
   "Mode line height backup.")
 
-(defvar smm-format-backup nil
+(defvar smm--format-backup nil
   "Mode line background.")
 
-(defun smm-save-and-revert-all-file-buffers ()
+;;;###autoload
+(defun smm-load-theme (theme &optional no-confirm no-enable)
+  (interactive
+   (list
+    (intern (completing-read "Load custom theme: "
+                             (mapcar 'symbol-name
+                                     (custom-available-themes))))
+    nil nil))
+  (load-theme theme no-confirm no-enable)
+
+  (if slim-modeline-mode
+      (smm--backup-bg-fg-color)))
+
+(defun smm--set-modeline-color (modeline-color)
+  "Set mode line color."
+  (unless smm-slim-modeline-color
+    (setq smm-slim-modeline-color modeline-color))
+  (set-face-attribute 'mode-line nil :background smm-slim-modeline-color)
+  (set-face-attribute 'mode-line nil :foreground smm-slim-modeline-color)
+  (set-face-attribute 'mode-line-inactive nil :background smm-slim-modeline-color)
+  (set-face-attribute 'mode-line-inactive nil :foreground smm-slim-modeline-color))
+
+(defun smm--backup-bg-fg-color ()
+  (setq smm--foreground-backup (face-attribute 'mode-line :foreground))
+  (setq smm--background-backup (face-attribute 'mode-line :background))
+  (setq smm--inactive-foreground-backup (face-attribute 'mode-line-inactive :foreground))
+  (setq smm--inactive-background-backup (face-attribute 'mode-line-inactive :background)))
+
+(defun smm--backup-other ()
+  (setq-default smm--format-backup  mode-line-format)
+  (setq smm--height-backup (face-attribute 'mode-line :height)))
+
+(defun smm-make-slim-modeline ()
+  (smm--backup-bg-fg-color)
+  (smm--backup-other)
+  (setq-default mode-line-format "")
+  (smm--set-modeline-color smm--background-backup)
+  (set-face-attribute 'mode-line nil :height 0.1)
+  (set-face-attribute 'mode-line-inactive nil :height 0.1))
+
+(defun smm--restore-format ()
+  (setq-default mode-line-format smm--format-backup))
+
+(defun smm--restore ()
+  (setq-default mode-line-format smm--format-backup)
+  (set-face-attribute 'mode-line nil :background smm--background-backup)
+  (set-face-attribute 'mode-line nil :foreground smm--foreground-backup)
+  (set-face-attribute 'mode-line-inactive nil :background smm--inactive-background-backup)
+  (set-face-attribute 'mode-line-inactive nil :foreground smm--inactive-foreground-backup)
+  (set-face-attribute 'mode-line nil :height smm--height-backup)
+  (set-face-attribute 'mode-line-inactive nil :height smm--height-backup))
+
+(defun smm--save-and-revert-all-file-buffers ()
   "Save and revert all file buffers. This function used to
  update `mode-line-format',  which is a buffer local variable."
   (save-some-buffers t)
@@ -72,36 +124,8 @@
   :global t
   (progn
     (if slim-modeline-mode
-        (progn
-          (setq-default smm-format-backup  mode-line-format)
-          (setq-default mode-line-format "")
-
-          (setq smm-height-backup (face-attribute 'mode-line :height))
-          (setq smm-foreground-backup (face-attribute 'mode-line :foreground))
-          (setq smm-background-backup (face-attribute 'mode-line :background))
-          (setq smm-inactive-foreground-backup (face-attribute 'mode-line-inactive :foreground))
-          (setq smm-inactive-background-backup (face-attribute 'mode-line-inactive :background))
-
-          (unless smm-slim-modeline-color
-            (setq smm-slim-modeline-color smm-background-backup))
-
-          (set-face-attribute 'mode-line nil :background smm-slim-modeline-color)
-          (set-face-attribute 'mode-line nil :foreground smm-slim-modeline-color)
-          (set-face-attribute 'mode-line-inactive nil :background smm-slim-modeline-color)
-          (set-face-attribute 'mode-line-inactive nil :foreground smm-slim-modeline-color)
-
-          (set-face-attribute 'mode-line nil :height 0.1)
-          (set-face-attribute 'mode-line-inactive nil :height 0.1))
-      (progn
-        (setq-default mode-line-format smm-format-backup)
-
-        (set-face-attribute 'mode-line nil :background smm-background-backup)
-        (set-face-attribute 'mode-line nil :foreground smm-foreground-backup)
-        (set-face-attribute 'mode-line-inactive nil :background smm-inactive-background-backup)
-        (set-face-attribute 'mode-line-inactive nil :foreground smm-inactive-foreground-backup)
-
-        (set-face-attribute 'mode-line nil :height smm-height-backup)
-        (set-face-attribute 'mode-line-inactive nil :height smm-height-backup)))
-    (smm-save-and-revert-all-file-buffers)))
+        (smm-make-slim-modeline)
+      (smm--restore))
+    (smm--save-and-revert-all-file-buffers)))
 
 (provide 'slim-modeline)
